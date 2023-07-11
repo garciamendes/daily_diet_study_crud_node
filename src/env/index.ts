17b03788372131973560e146b35171b433c6c2cc
mmendes_mph <mmendes@morphus.com.br>
2023-07-10 22:01:22 -0300

    Teste

--- src/env/index.ts
+++ src/env/index.ts
@@ -12,7 +12,7 @@ const envSchema = zod.object({
   NODE_ENV: zod.enum(['development', 'production', 'test']).default('production'),
   DATABASE_CLIENT: zod.enum(['sqlite', 'pg']),
   DATABASE_URL: zod.string(),
-  PORT: zod.coerce.number().default(3001),
+  PORT: zod.coerce.number().default(3000),
   ALLOWED_ORIGINS: zod.string(),
   JWT_SECRET: zod.string()
 })
